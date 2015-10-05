9426e325dc3ebf4d75735202fb8e2e220ef4175c
Jeremy Venegas <jeremymvenegas@gmail.com>
2015-10-04 23:48:56 -0700

    basic functions of nav and side menu for mobile

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,34 @@
+$(document).ready(function() {
+	var toggleButton = $('.menu-controller');
+	var body = $('body');
 
+	$.fn.hasAttr = function(name) {
+		return this.attr(name) !== undefined;
+	}
+
+  function removeClasses(classPrefix) {
+    var bodyHasClasses = body.hasAttr('class');
+
+    if (bodyHasClasses) {
+      var classes = body.attr("class").split(" ");
+      $.each(classes, function(index) {
+        if (classes[index].indexOf(classPrefix) == 0) {
+          $('body').removeClass(classes[index]);
+        }
+      });
+    }
+  }
+
+  $(toggleButton).click(function() {
+    thisId = $(this).attr('id');
+    classToAdd = 'active-' + thisId;
+    alreadyAdded = body.hasClass(classToAdd);
+    if (alreadyAdded) {
+    	body.removeClass(classToAdd);
+    }
+    else {
+    	removeClasses('active-');
+    	body.addClass(classToAdd);
+    }
+  });
+});
