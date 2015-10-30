3e3785b5db2c1e92a57edbe9e3787d1b7d970291
Jeremy Venegas <jeremymvenegas@gmail.com>
2015-10-30 11:40:59 -0700

    its been tou long

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
 
   // EZ tabs 
   var eztLink = $( ".ezt-nav a" );
+  var eztRadio = $( ".ezt-nav-radio label" );
+
+  var advLink = $( ".u-advanced-link" );
+  var socialLink = $( ".u-social-toggle" );
 
   $(eztLink).click(function() {
     var ezTarget = $(this).closest('ul').attr('ezt');
@@ -20,6 +24,29 @@ $(document).ready(function() {
       $(ezTargetSelector + '.ezt-content').removeClass('ezt-active');
       $(ezTargetSelector + '.ezt-content:nth-child(' + ezIndex + ')').addClass('ezt-active');
     }            
+  });
+
+  $(eztRadio).click(function() {
+    var ezTarget = $(this).closest('.ezt-nav-radio').attr('ezt');
+    var ezTargetSelector = '[ezt="' + ezTarget + '"] ';
+    var ezIndex = $(this).parent().index() + 1; 
+
+    //update content
+    $(ezTargetSelector + '.ezt-content').removeClass('ezt-active');
+    $(ezTargetSelector + '.ezt-content:nth-child(' + ezIndex + ')').addClass('ezt-active');      
+  });
+
+  $(advLink).click(function() {
+    $(this).toggleClass('open');
+    $(this).next().toggleClass('open');
+  });
+
+  $(socialLink).click(function() {
+    var socialTarget = $(this).attr("social-target");
+    var ezTargetSelector = '[social-target="' + socialTarget + '"] ';
+    console.log('casul');
+    //update content
+    $(ezTargetSelector).toggleClass('show');   
   });
 
   // initialize selects
